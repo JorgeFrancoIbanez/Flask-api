f2df79092e13e97c9727044a8d8ed9fc2e6c9ffe
jorge.franco <jorge.franco@ormuco.com>
2018-10-21 22:21:22 -0500

    changes on pre-requisites

--- run.sh
+++ run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-
-
+virtualenv="Flask";
 #export DATABASE_URL=mysql+pymysql://root:root@localhost:3306/col
+#install mysql 
+#install sudo apt-get install libmysqlclient-dev  or  sudo apt install default-libmysqlclient-dev
+
 export FLASK_APP=cam.py
-source ~/venvs/Flask/bin/activate
-flask run
+if [ ! -d "$virtualenv" ]; then
+	virtualenv --system-site-packages $virtualenv
+	echo virtualenv created;
+	source $virtualenv/bin/activate;
+	pip install --upgrade setuptools
+        sudo apt-get install libmysqlclient-dev python-dev
+	pip install -r requirements.txt
+	echo "requirements were installed";
+else
+	source "$virtualenv/bin/activate";
+	flask run;
+fi
