4ce14400a04ad95a177fa51d9f21217ca69adb91
JorgeFrancoIbanez <jorgefrancoibanez@hotmail.com>
2019-06-12 08:17:50 -0500

    saving stuff

--- scripts/sentpost.py
+++ scripts/sentpost.py
@@ -1,8 +1,8 @@
 import requests
 import json
-# test = json.dumps({"key": "value","vgvghvhv": [{"guhi": "jkjhkjh","uhuihuih":"jkjoijo"}],"jkjl": "jkhkj"})
-# r = requests.post('http://127.0.0.1:5000/post/e10415b78ad05b04c695ea239267dd0523afd8b2c713108f84ad446d26208bd5',  data=test)
-# print(r.text)
-
-r = requests.get('http://127.0.0.1:5000/get-file')
+test = json.dumps({"key": "value","vgvghvhv": [{"guhi": "jkjhkjh","uhuihuih":"jkjoijo"}],"jkjl": "jkhkj"})
+r = requests.post('http://testtest.ml/post/e10415b78ad05b04c695ea239267dd0523afd8b2c713108f84ad446d26208bd5',  data=test)
 print(r.text)
+
+# r = requests.get('http://127.0.0.1:5000/get-file')
+# print(r.text)
